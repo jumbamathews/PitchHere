b1652c3a2a1d7333852bb57b8483f6a423b67789
wanjiiru <liz240@live.com>
2018-06-24 14:19:12 +0300

    add forms module

---
+++ app/main/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,SubmitField,TextAreaField,RadioField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
+
+
+
+class PitchForm(FlaskForm):
+	body = TextAreaField("whats your pitch ?",validators=[Required()])
+	category = RadioField('Label', choices=[('pickup lines','pickup lines'), ('interview pitch','interview pitch'),('product pitch','product pitch'),('promotion pitch','promotion pitch')])
+	submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+	body = TextAreaField('',validators=[Required()])
+	submit = SubmitField()
