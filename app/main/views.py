a08f13b3daa58da30e5abd7e4281a1b8b996f0ac
wanjiiru <liz240@live.com>
2018-07-03 08:24:32 +0300

    add upvotes and downvote routes

--- app/main/views.py
+++ app/main/views.py
@@ -1,30 +1,31 @@
 from flask import render_template,request,redirect,url_for,abort, flash
 from . import main
 from flask_login import login_required, current_user
-from ..models import Pitch, User,Comment
-from .forms import PitchForm, CommentForm
+from ..models import Pitch, User,Comment,Upvote,Downvote
+from .forms import PitchForm, CommentForm, UpvoteForm
 from flask.views import View,MethodView
 from .. import db
 import markdown2
 
 
 # Views
-@main.route('/')
+@main.route('/', methods = ['GET','POST'])
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-
+    pitch = Pitch.query.filter_by().first()
     title = 'Home'
-    pitch = Pitch.query.filter_by().all()
     pickuplines = Pitch.query.filter_by(category="pickuplines")
     interviewpitch = Pitch.query.filter_by(category = "interviewpitch")
     promotionpitch = Pitch.query.filter_by(category = "promotionpitch")
     productpitch = Pitch.query.filter_by(category = "productpitch")
+
+    upvotes = Upvote.get_all_upvotes(pitch_id=Pitch.id)
     
 
-    return render_template('home.html', title = title,pitch = pitch,pickuplines=pickuplines, interviewpitch= interviewpitch, promotionpitch = promotionpitch, productpitch = productpitch)
+    return render_template('home.html', title = title, pitch = pitch, pickuplines=pickuplines, interviewpitch= interviewpitch, promotionpitch = promotionpitch, productpitch = productpitch, upvotes=upvotes)
     
 
 
@@ -34,12 +35,14 @@ def index():
 @login_required
 def new_pitch():
     form = PitchForm()
+    my_upvotes = Upvote.query.filter_by(pitch_id = Pitch.id)
     if form.validate_on_submit():
         description = form.description.data
+        title = form.title.data
         owner_id = current_user
         category = form.category.data
         print(current_user._get_current_object().id)
-        new_pitch = Pitch(owner_id =current_user._get_current_object().id,description=description,category=category)
+        new_pitch = Pitch(owner_id =current_user._get_current_object().id, title = title,description=description,category=category)
         db.session.add(new_pitch)
         db.session.commit()
         
@@ -66,27 +69,45 @@ def new_comment(pitch_id):
         return redirect(url_for('.new_comment', pitch_id= pitch_id))
 
     all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
-    return render_template('comments.html', form = form,comment = all_comments )
+    return render_template('comments.html', form = form, comment = all_comments, pitch = pitch )
 
 
-@main.route('/pitch/upvote/<int:id>')
+@main.route('/pitch/upvote/<int:pitch_id>/upvote', methods = ['GET', 'POST'])
 @login_required
-def upvote(id):
-	pitch = Pitch.query.filter_by(id=id)
-	upvotes = pitch.upvotes + 1
-	db.session.add(pitch)
-	db.session.commit()
+def upvote(pitch_id):
+    pitch = Pitch.query.get(pitch_id)
+    user = current_user
+    pitch_upvotes = Upvote.query.filter_by(pitch_id= pitch_id)
+    
+    if Upvote.query.filter(Upvote.user_id==user.id,Upvote.pitch_id==pitch_id).first():
+        return  redirect(url_for('main.index'))
 
-	return redirect(url_for('main.index'))
 
-@main.route('/pitch/downvote/<int:id>')
-def downvote(id):
-	pitch = Pitch.query.filter_by(id=id)
-	pitch.downvotes = pitch.downvotes + 1
-	db.session.add(pitch)
-	db.session.commit()
+    new_upvote = Upvote(pitch_id=pitch_id, user = current_user)
+    new_upvote.save_upvotes()
+    return redirect(url_for('main.index'))
 
-	return redirect(url_for('main.index'))
+
+
+#    new_upvote = Upvote(user=current_user, pitch=pitch, vote_number=1)
+#    new_vote.save_vote()
+# return redirect(url_for('main.index'))
+
+
+@main.route('/pitch/downvote/<int:pitch_id>/downvote', methods = ['GET', 'POST'])
+@login_required
+def downvote(pitch_id):
+    pitch = Pitch.query.get(pitch_id)
+    user = current_user
+    pitch_downvotes = Downvote.query.filter_by(pitch_id= pitch_id)
+    
+    if Downvote.query.filter(Downvote.user_id==user.id,Downvote.pitch_id==pitch_id).first():
+        return  redirect(url_for('main.index'))
+
+
+    new_downvote = Downvote(pitch_id=pitch_id, user = current_user)
+    new_downvote.save_downvotes()
+    return redirect(url_for('main.index'))
 
 
 		
