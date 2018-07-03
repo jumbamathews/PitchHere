77e3642e9ec47d9778a14c00a85715c4ce71fbb2
wanjiiru <liz240@live.com>
2018-07-03 16:57:34 +0300

    Deploying to heroku

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 export DATABASE_URL='postgres://djrlzmirlqyozz:225c4190ab3e169401c0066986eebf3d033f9924c04a76202cad869ededac866@ec2-54-235-220-220.compute-1.amazonaws.com:5432/d24vkpns1uaqp9'
 
+export SECRET_KEY = 'powerfulsecretkey'
+
 python3.6 manage.py server
