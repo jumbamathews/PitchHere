2750e4662d7a140b6055e74dcdef347dac4dbcf8
MATHEWS_JUMBA <mathewsjumba19@outlook.com>
2019-11-24 20:46:28 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export DATABASE_URL='postgres://postgres://bbxavrdcudveex:f0df9ab72a632f16768dc68326ad97a436ef815275917295fdc449319cb6d4c4@ec2-54-225-173-42.compute-1.amazonaws.com:5432/d99c9v8mpmm2ir'
+export DATABASE_URL='postgres://bbxavrdcudveex:f0df9ab72a632f16768dc68326ad97a436ef815275917295fdc449319cb6d4c4@ec2-54-225-173-42.compute-1.amazonaws.com:5432/d99c9v8mpmm2ir'
 
 export SECRET_KEY='powerfulsecretkey'
 
